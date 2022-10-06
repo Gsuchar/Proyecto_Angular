479a6c3bacd961bee77cdf16f5a52a2a62cbb636
gsuchar <jugasu06@gmail.com>
2022-10-06 20:07:41 -0300

    Desafio_Forms.

--- Forms_Sucharzewski/src/app/componentes/forms/forms.component.ts
+++ Forms_Sucharzewski/src/app/componentes/forms/forms.component.ts
@@ -8,38 +8,29 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class FormsComponent implements OnInit {
   formularioUser: FormGroup;
-  //controlPasswordsIguales!: boolean;
+  controlPasswordsIguales!:boolean;
+  
+  //static cntrlPassIguales: boolean = false;
   constructor(
     private fb: FormBuilder
   ) { 
     this.formularioUser = fb.group({
       nombre: new FormControl('', [Validators.required]),
       apellido: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required, Validators.email]),
+      email: new FormControl('', [Validators.pattern('^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$'), Validators.required]),
       password: new FormControl('', [Validators.required, Validators.minLength(5)]),
       password2: new FormControl('', [Validators.required, Validators.minLength(5)]),
-      admin: new FormControl(false, []),
-    })
+      admin: new FormControl(false, [])});
   }
 
   ngOnInit(): void {
   }
 
   agregarUser(){
-    alert(this.controlPasswordsIguales)
-    console.log(this.formularioUser)
-    //const Fpassword = this.formularioUser.value.password;
-    //const Fpassword2 = this.formularioUser.value.password2;
-    //this.controlPasswordsIguales = Fpassword === Fpassword2;
-  }
-   controlPasswordsIguales = (iguales: boolean) : boolean => {
+     console.log(this.formularioUser)
     const Fpassword = this.formularioUser.value.password;
-    const Fpassword2 = this.formularioUser.value.password2;
-    if (Fpassword === Fpassword2){ 
-    return (iguales);
-  }else {return (iguales==false);}
-};
-//ngAfterViewInit() {
-//  this.renderer.listen(this.elementRef.nativeElement, 'click', (event) => { this.controlPasswordsIguales;});
-//}
+    const Fpassword2 = this.formularioUser.value.password2;    
+    this.controlPasswordsIguales = Fpassword === Fpassword2;
+  }
+  
 }
