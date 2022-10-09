1357f4cd8f01454ad9df69f7a3d8e6d200c54d00
Gaston Sucharzewski <jugasu06@gmail.com>
2022-10-08 22:59:33 -0300

    PF_1_v0.1

---
+++ PF_1-Sucharzewski/src/app/material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { MatTableModule } from "@angular/material/table";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+@NgModule({
+    imports:[
+        MatTableModule,
+        MatCardModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule
+    ],
+    exports:[
+        MatTableModule,
+        MatCardModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule
+    ]
+})
+export class MaterialModule{}
