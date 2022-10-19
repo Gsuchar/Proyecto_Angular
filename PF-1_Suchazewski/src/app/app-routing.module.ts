c2ae25b285c2893d491b97aa5a2dac7b645d37ca
Gaston Sucharzewski <jugasu06@gmail.com>
2022-10-18 22:58:02 -0300

    .

--- PF-1_Suchazewski/src/app/app-routing.module.ts
+++ PF-1_Suchazewski/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AlumnoGdComponent } from './Componentes/gestionDatos/alumno-gd/alumno-gd.component';
 
 const routes: Routes = [
   //aca asigno ruteo
-  { path: '', component: AppComponent }
+  { path: 'alumnos', component: AlumnoGdComponent }
 ];
 
 @NgModule({
